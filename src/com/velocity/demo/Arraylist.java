952362342ce63acd232dfda70a30675758dec39b
udaytakawale <tudayp940@gmail.com>
2022-12-24 19:43:34 +0530

    first commit : added arraylist demo code

---
+++ src/com/velocity/demo/Arraylist.java
@@ -0,0 +1,13 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+public static void main(String[] args) {
+	ArrayList<String> list = new ArrayList<String> ();
+	list.add("sohan");
+	list.add("mohan");
+	list.add("ganesh");
+	System.out.println(list);
+}
+}
